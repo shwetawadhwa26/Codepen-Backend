59e87c725e82ec4020a6c3a2004be99ed25840e9
SHWETA WADHWA <125170830+shwetawadhwa26@users.noreply.github.com>
2024-09-01 14:21:14 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,13 @@
+let {MongoClient}= require('mongodb');
+const url = "mongodb://localhost:27017";
+const clientObj = new MongoClient(url);
+let db="";
+try{
+	db = clientObj.db("Codepen");
+}
+catch(err){
+	console.log(err);
+	throw err;
+}
+
+module.exports = db;
